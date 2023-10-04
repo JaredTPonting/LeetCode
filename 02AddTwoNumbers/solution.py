9db93477e454a48d2ee1d7690eef96008bbba54f
Jared <jaredtponting@gmail.com>
2023-10-04 12:24:40 +0100

    changes

---
+++ 02AddTwoNumbers/solution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        var = None
+        return var
+
+
+l1 = ListNode()
+l2 = ListNode()
+
+j = Solution()
+j.addTwoNumbers(l1, l2)
